f6227233c218a8e8cf652270b1c491ab7765b5d1
CristianoNunes <cristianonunes99@gmail.com>
2023-01-30 14:47:40 -0300

    Add screen SignUp

--- App.tsx
+++ App.tsx
@@ -5,11 +5,10 @@ import {
   Roboto_400Regular,
   Roboto_700Bold,
 } from "@expo-google-fonts/roboto";
-
 import { THEME } from "./src/theme";
 
 import { Loading } from "@components/Loading";
-import { SignIn } from "@screens/SignIn";
+import { SignUp } from "@screens/SignUp";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
@@ -20,7 +19,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      {fontsLoaded ? <SignIn /> : <Loading />}
+      {fontsLoaded ? <SignUp /> : <Loading />}
     </NativeBaseProvider>
   );
 }
