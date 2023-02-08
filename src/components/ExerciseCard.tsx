a962ff32f083f6be28289e687f60c28a9f2128c5
CristianoNunes <cristianonunes99@gmail.com>
2023-02-07 21:37:21 -0300

    Finished screen History

--- src/components/ExerciseCard.tsx
+++ src/components/ExerciseCard.tsx
@@ -30,7 +30,7 @@ export function ExerciseCard({ ...rest }: Props) {
 
         <VStack flex={1}>
           <Heading fontSize="lg" color="white">
-            Remanda unilateral
+            Remada unilateral
           </Heading>
 
           <Text fontSize="sm" color="gray.200" mt={1} numberOfLines={2}>
