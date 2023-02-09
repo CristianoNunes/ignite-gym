d0abd44da1dc2287c2cc07384661163f98a5da85
CristianoNunes <cristianonunes99@gmail.com>
2023-02-09 15:57:57 -0300

    Developed authentication

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,19 +1,69 @@
+import { useState } from "react";
+import { Controller, useForm } from "react-hook-form";
 import { useNavigation } from "@react-navigation/native";
-import { VStack, Image, Text, Center, Heading, ScrollView } from "native-base";
+import {
+  VStack,
+  Image,
+  Text,
+  Center,
+  Heading,
+  ScrollView,
+  useToast,
+} from "native-base";
 
 import { AuthNavigatorRoutesProps } from "@routes/auth.routes";
+import { useAuth } from "@hooks/useAuth";
 
 import LogoSvg from "@assets/logo.svg";
 import BackgroundImg from "@assets/background.png";
+
+import { AppError } from "@utils/AppError";
+
 import { Input } from "@components/Input";
 import { Button } from "@components/Button";
 
+type FormData = {
+  email: string;
+  password: string;
+};
+
 export function SignIn() {
+  const [isLoading, setIsLoading] = useState(false);
+
+  const { singIn } = useAuth();
+  const toast = useToast();
   const navigation = useNavigation<AuthNavigatorRoutesProps>();
+
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>();
 
   const handleNewAccount = () => {
     navigation.navigate("signUp");
   };
+
+  async function handleSignIn({ email, password }: FormData) {
+    try {
+      setIsLoading(true);
+      await singIn(email, password);
+    } catch (error) {
+      const isAppError = error instanceof AppError;
+
+      const title = isAppError
+        ? error.message
+        : "Não foi possível entrar. Tente novamente mais tarde.";
+
+      setIsLoading(false);
+
+      toast.show({
+        title,
+        placement: "top",
+        bgColor: "red.500",
+      });
+    }
+  }
 
   return (
     <ScrollView
@@ -42,14 +92,36 @@ export function SignIn() {
             Acesse a conta
           </Heading>
 
-          <Input
-            placeholder="E-mail"
-            keyboardType="email-address"
-            autoCapitalize="none"
+          <Controller
+            control={control}
+            name="email"
+            rules={{ required: "Informe o e-mail" }}
+            render={({ field: { onChange } }) => (
+              <Input
+                placeholder="E-mail"
+                keyboardType="email-address"
+                autoCapitalize="none"
+                onChangeText={onChange}
+                errorMessage={errors.email?.message}
+              />
+            )}
           />
-          <Input placeholder="Senha" secureTextEntry />
 
-          <Button title="Acessar" />
+          <Controller
+            control={control}
+            name="password"
+            rules={{ required: "Informe a senha" }}
+            render={({ field: { onChange } }) => (
+              <Input
+                placeholder="Senha"
+                secureTextEntry
+                onChangeText={onChange}
+                errorMessage={errors.password?.message}
+              />
+            )}
+          />
+
+          <Button title="Acessar" onPress={handleSubmit(handleSignIn)} />
         </Center>
 
         <Center mt={24}>
@@ -62,6 +134,7 @@ export function SignIn() {
           title="Criar Conta"
           variant="outline"
           onPress={handleNewAccount}
+          isLoading={isLoading}
         />
       </VStack>
     </ScrollView>
